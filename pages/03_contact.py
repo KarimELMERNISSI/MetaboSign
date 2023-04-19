b4b17698cceaf3ac02b30810305f829d8a8a890b
KarimELMERNISSI <karim.elmernissi.75@gmail.com>
2023-04-19 17:45:51 +0200

    contact form

--- pages/03_contact.py
+++ pages/03_contact.py
@@ -1,1 +1,25 @@
 
+import streamlit as st  # pip install streamlit
+
+st.header(":mailbox: Laissez nous un message")
+
+
+contact_form = """
+<form action="https://formsubmit.co/KARIM.EVASION@GMAIL.COM" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Votre nom" required>
+     <input type="email" name="email" placeholder="Votre email" required>
+     <textarea name="message" placeholder="Votre message"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("./style/style.css")
